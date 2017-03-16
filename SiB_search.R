23b31d04492514a30a7ccd321a7d92b0426ef522
Juan Rey <biorey87@gmail.com>
2017-03-16 10:29:20 -0500

    Create SiB_search.R

---
+++ SiB_search.R
@@ -0,0 +1,37 @@
+# Trabajar sobre 
+setwd("C:/Users/juan.rey/Desktop")
+
+#Abrir base de datos actualizada cada martes 
+sib <- read.table("occurrence-search-1281-1489495590886.txt", sep = "\t", 
+                  header = T, fill = T, encoding="UTF-8")
+
+#Guardo base de datos sib en RDS
+saveRDS(sib, file = "SIB_14-03-17.rds")
+#Abro RSD SiB
+sib<- readRDS("SIB_14-03-17.rds")
+#================================
+#Consultas
+#=============================
+
+#consulta por departamento (es necesario revisar el nombre de los departamentos)
+inv<- table(sib$Departamento)
+a<- data.frame(inv)
+#Una vez se identifica los departamentos a consultar se realiza consulta en esta caso es Arauca
+dep <- subset(sib, subset=sib$Departamento== "ARAUCA" | sib$Departamento== "Arauca")
+
+
+#consulta por especies (es necesario revisar el nombre de las especies
+inv<- table(sib$Nombre.científico)
+a<- data.frame(inv)
+#Una vez se identifica los departamentos a consultar se realiza consulta en esta caso es Arauca
+falco <- subset(sib, subset=sib$Nombre.científico== "Falco deiroleucus")
+
+# Si la consulta es por familias
+sppcsib <- subset(sib, subset=sib$Familia== "Apidae" | sib$Familia== "Colletidae" 
+                  | sib$Familia== "Andrenidae"| sib$Familia== "Halictidae" 
+                  | sib$Familia== "Megachilidae")
+
+
+#exporto mi consulta en una tabla
+write.table(falco, file = "Falco_deiroleucus_sib.csv", sep = ",", col.names = NA,
+            qmethod = "double")
